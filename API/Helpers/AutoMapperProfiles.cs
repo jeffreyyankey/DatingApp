d69504566a827b94aaace206713484de00a9c7fa
Jeffrey Yankey <jeffrey.yankey@outlook.com>
2025-02-10 22:26:23 -0500

    End of Section 8

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,18 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers;
+public class AutoMapperProfiles : Profile
+{
+    public AutoMapperProfiles()
+    {
+        // Using ! to tell the compiler that automapper will set Photos to null rather than throwing an exception
+        CreateMap<AppUser, MemberDto>()
+            .ForMember(d => d.Age, o => o.MapFrom(s => s.DateOfBirth.CalculateAge()))
+            .ForMember(d => d.PhotoUrl, 
+                o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url));
+        CreateMap<Photo, PhotoDto>();
+    }
+}
